c6203a44190f42ca7070ac15a47d4c5ebad331ea
Giuseppe Davide Salvatore Pomo <giuseppepomo8@gmail.com>
2025-01-20 16:33:15 +0100

    secondo commit

--- src/components/MainSearch.jsx
+++ src/components/MainSearch.jsx
@@ -1,10 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Container, Row, Col, Form } from "react-bootstrap";
 import Job from "./Job";
+import { Link } from "react-router-dom";
 
 const MainSearch = () => {
   const [query, setQuery] = useState("");
   const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const baseEndpoint =
     "https://strive-benchmark.herokuapp.com/api/jobs?search=";
@@ -13,30 +16,46 @@ const MainSearch = () => {
     setQuery(e.target.value);
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-
-    try {
-      const response = await fetch(baseEndpoint + query + "&limit=20");
-      if (response.ok) {
-        const { data } = await response.json();
-        setJobs(data);
-      } else {
-        alert("Error fetching results");
-      }
-    } catch (error) {
-      console.log(error);
+  useEffect(() => {
+    if (query.trim() === "") {
+      setJobs([]);
+      return;
     }
-  };
+
+    setLoading(true);
+    setError(null);
+
+    const fetchJobs = async () => {
+      try {
+        const response = await fetch(baseEndpoint + query + "&limit=20");
+        const result = await response.json();
+        if (response.ok) {
+          setJobs(result.data);
+        } else {
+          setError("Error fetching results");
+        }
+      } catch (error) {
+        setError("Failed to fetch data. Please try again.");
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    const debounceTimeout = setTimeout(fetchJobs, 500);
+
+    return () => clearTimeout(debounceTimeout);
+  }, [query]);
+  f;
 
   return (
     <Container>
       <Row>
         <Col xs={10} className="mx-auto my-3">
           <h1 className="display-1">Remote Jobs Search</h1>
+          <Link to="/favourites">Vai ai preferiti</Link>
         </Col>
         <Col xs={10} className="mx-auto">
-          <Form onSubmit={handleSubmit}>
+          <Form>
             <Form.Control
               type="search"
               value={query}
@@ -45,10 +64,13 @@ const MainSearch = () => {
             />
           </Form>
         </Col>
+
         <Col xs={10} className="mx-auto mb-5">
-          {jobs.map((jobData) => (
-            <Job key={jobData._id} data={jobData} />
-          ))}
+          {loading && <p>Loading...</p>}
+          {error && <p>{error}</p>}
+          {jobs.length > 0
+            ? jobs.map((jobData) => <Job key={jobData._id} data={jobData} />)
+            : !loading && <p>No results found.</p>}
         </Col>
       </Row>
     </Container>
